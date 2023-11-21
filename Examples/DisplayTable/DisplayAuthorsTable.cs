83d895563671fe40b0b6d361b43a1c9e5ec678e3
avgra3 <antonygradillas@gmail.com>
2023-11-21 10:42:50 -0700

    Started example with Databses and LINQ

---
+++ Examples/DisplayTable/DisplayAuthorsTable.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Validation;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DisplayTable
+{
+    public partial class DisplayAuthorsTable : Form
+    {
+        // Constructor
+        public DisplayAuthorsTable()
+        {
+            InitializeComponent();
+        }
+
+        // Entity Framework DbContext
+        private BookExamples.BooksEntities dbcontext = new BookExamples.BooksEntities();
+
+        // Load data from database into DataGridView
+        private void DisplayAuthorsTable_Load(object sender, EventArgs e)
+        {
+            // Load Authors table ordered by LastName then FirstName
+            dbcontext.Authors
+                .OrderBy(author => author.LastName)
+                .ThenBy(author => author.FirstName)
+                .Load();
+
+            // Specify DataSource for authorBindingSource
+            authorBindingSource.DataSource = dbcontext.Authors.Local;
+        }
+
+        // Click event handler for the Save Button in the
+        // BindingNavigator saves the changes made to the data
+        private void authorBindingNavigatorSaveItem_Click(object sender, EventArgs e)
+        {
+            Validate(); // Validate the input fields
+            authorBindingSource.EndEdit(); // Complete current edit, if any
+
+            // Try to save changes
+            try
+            {
+                dbcontext.SaveChanges(); // Write changes to database file
+            }
+            catch (DbEntityValidationException)
+            {
+                MessageBox.Show("FirstName and LastName must contain values", "Entity Validation Exception");
+            }
+        }
+    }
+}
